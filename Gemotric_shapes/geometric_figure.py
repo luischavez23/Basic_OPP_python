3511479dca815ca132b3e49e790946b4f7811a32
luischavez23 <lchavezdiazdesandi@gmail.com>
2021-11-30 20:45:20 -0600

    Learn how to use multiple inheritance python

---
+++ Gemotric_shapes/geometric_figure.py
@@ -0,0 +1,26 @@
+class Geometric:
+    
+    def __init__(self, width, length):
+        self._width = width
+        self._length = length
+
+    @property
+    def width(self):
+        return self._width
+    
+    @width.setter
+    def width(self, width):
+        self._width = width
+    
+    @property
+    def length(self):
+        return self._length
+    
+    @length.setter
+    def length(self, length):
+        self._length = length
+
+
+
+
+
